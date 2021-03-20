583f3c458ca8353c91c9231715c552afe35ad8f0
sujang958 <enter7377@naver.com>
2021-03-20 17:31:39 +0900

    asdf

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { config } = require('dotenv');
 const cookie = require('cookie-parser');
 const body = require('body-parser');
 const Mail = require('./functions/send');
+const UUID = require('uuid').v4;
 
 // Variables
 config();
@@ -14,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 // App
 const cryptr = new Cryptr(process.env.ASDF);
 const app = express();
-const mailer = new Mail('enter7377', cryptr.decrypt(process.env.MAIL_PW));
+const mailer = new Mail('enter7377@naver.com', cryptr.decrypt(process.env.MAIL_PW));
 const DBClient = new MongoDB.MongoClient(`mongodb+srv://user:${cryptr.decrypt(process.env.DB_PW)}@cluster0.i3cgq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -37,6 +38,9 @@ app.use(body.json());
 app.use(express.json());
 app.use(cookie());
 app.use(express.static('public'));
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
 
 // Route
 app.get('/', async (req, res) => {
@@ -56,14 +60,37 @@ app.post('/sign_in', async (req, res) => {
     const { email } = req.body;
 
     if (!email) return res.send('<script>alert("이메일을 입력해주세요!");</script>').redirect('/');
-
     let userDB = await app.db.user.findOne({_id: email});
     if (userDB) return res.send('<script>alert("이미 가입한 이메일입니다!");</script>').redirect('/');
-
+    
+    let i = 0;
+    let code = '';
+    while (i < 5) {
+        code = UUID();
+        var checkDB = await app.db.user.findOne({code: code});
+        if (!checkDB) i += 100;
+        else continue;
+    }
+    
     await app.db.user.insertOne({
         _id: email,
         auth: false,
+        code,
     });
+    
+    mailer.send(mailer.createMailOption(email, '[NotePad] 이메일 인증', `http://localhost:3000/verify/${code}\n이 링크를 다른사람에게 보여주지마세요`));
+    return res.send('<script>alert("이메일로 인증 메일을 보냈습니다, 확인해주세요");</script>').redirect('/');
+});
+
+app.get('/verify/:code', async (req, res) => {
+    const { code } = req.params;
+    if (!code)
+        return res.send('<script>alert("이메일을 입력해주세요!");</script>').redirect('/');
+    let userDB = await app.db.user.findOne({code: code});
+    if (!userDB)
+        return res.send('<script>alert("이메일을 입력해주세요!");</script>').redirect('/');
+    if (userDB.auth)
+        return res.send('<script>alert("이미 인증된 계정입니다!");</script>').redirect('/');
 });
 
 // Listen
